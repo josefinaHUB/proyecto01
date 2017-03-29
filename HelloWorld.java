3e79c0fd4131b9678517027a178780a5e169497b
Josefina Hdz <josefina.hernandez@softtek.com>
2017-03-29 17:12:04 -0600

    5 commit ultimo

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,6 @@ package paquete01;
 public class HelloWorld {
 
 		  public static void main(String[] args){
-		    System.out.println("Hello Git and GitHUB!");
+		    System.out.println("Hello Git and GitHub, a bug was fixed!!");
 		  }
 }
