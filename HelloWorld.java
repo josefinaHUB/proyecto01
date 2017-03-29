6c357b939f08e62a56c71649ac5719fe43338169
josefinaHUB <josefina.hernandez@softtek.com>
2017-03-29 16:43:18 -0600

    2o commit Modificación para Hello GitHUB
    ejercicio para sincronizar

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,6 @@ package paquete01;
 public class HelloWorld {
 
 		  public static void main(String[] args){
-		    System.out.println("Hello Git!");
+		    System.out.println("Hello GitHUB!");
 		  }
 }
